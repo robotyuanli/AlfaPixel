61eefe2c9dbb664e618e4f8a99277a045884dcc3
robotyuanli <robotyuanli@outlook.com>
2021-07-19 15:29:04 +0800

    api-update

--- app/config/setting.js
+++ app/config/setting.js
@@ -2,7 +2,7 @@
  * Basic Setting Variables Define
  */
 export const BaseSetting = {
-  name: 'AlfaPixel',
-  displayName: 'AlfaPixel',
-  appVersion: '1.0.1',
+  name: 'מזמור המנורה כולל באורים',
+  displayName: 'מזמור המנורה כולל באורים',
+  appVersion: '1.0.0',
 };
--- app/services/userServices.js
+++ app/services/userServices.js
@@ -4,7 +4,7 @@ import { api1, api2, api3 } from '../../app.json';
 // Category List
 function getCategories(id) {
   if (id === 0) {
-    return ApiClient.get(api1);
+    return ApiClient.get(api2);
   } else {
     return ApiClient.get('/subjects/subject/' + id);
   }
